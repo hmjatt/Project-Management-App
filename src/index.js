af5273224d97b60ff695742876433b75d37f82b4
Harmeet Matharoo <hmjatt@gmail.com>
2022-05-06 17:03:52 -0600

    create project button WIP

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import './style/styles.css';
 // import gitImage from './images/git-logo.png';
 
+let addProjectButton = document.getElementById('addProjectButton');
+
+
+
+addProjectButton.addEventListener('click', openForm);
+
+function openForm() {
+	document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+	document.getElementById("myForm").style.display = "none";
+}
 
 console.log("it works");
