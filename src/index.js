827ee0641016d560c0f4cf0ea77602186d19ff1d
Harmeet Matharoo <hmjatt@gmail.com>
2022-05-06 20:24:07 -0600

    form style complete

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import './style/styles.css';
 // import gitImage from './images/git-logo.png';
 
 let addProjectButton = document.getElementById('addProjectButton');
+let projectSubmitButton = document.getElementById('submitBtn');
 
 
 
 addProjectButton.addEventListener('click', openForm);
+projectSubmitButton.addEventListener('click', closeForm);
 
 function openForm() {
 	document.getElementById("myForm").style.display = "block";
+	addProjectButton.style.display = "none";
 }
 
 function closeForm() {
