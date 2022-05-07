604224aa8574ef5483ed8957c4f5c5d58a0109a0
Harmeet Matharoo <hmjatt@gmail.com>
2022-05-06 21:04:25 -0600

    added placeholder tasks

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import './style/styles.css';
 // import gitImage from './images/git-logo.png';
 
+//dom stuff
+
 let addProjectButton = document.getElementById('addProjectButton');
 let projectSubmitButton = document.getElementById('submitBtn');
+let inputMenu = document.getElementById('inputMenu');
+let taskList = document.getElementById('taskList');
+let addTaskButton = document.getElementById('addTaskButton');
+let taskSubmitButton = document.getElementById('taskSubmitButton');
 
 
-
+inputMenu.addEventListener('click', addMargin);
+addTaskButton.addEventListener('click', addTask);
 addProjectButton.addEventListener('click', openForm);
 projectSubmitButton.addEventListener('click', closeForm);
+taskSubmitButton.addEventListener('click', closeTaskForm);
+
 
 function openForm() {
 	document.getElementById("myForm").style.display = "block";
@@ -18,4 +27,17 @@ function closeForm() {
 	document.getElementById("myForm").style.display = "none";
 }
 
-console.log("it works");
+function addMargin() {
+	taskList.classList.toggle('addmarginAfterClick');
+	console.log("it works");
+}
+
+function addTask() {
+	document.getElementById("myTasks").style.display = "block";
+	addTaskButton.style.display = "none";
+	
+}
+
+function closeTaskForm() {
+	document.getElementById("myTasks").style.display = "none";
+}
