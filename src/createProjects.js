84e0dddc569d8cbd1f7053dcff531ec64243480f
Harmeet Matharoo <hmjatt@gmail.com>
2022-05-09 23:42:01 -0600

    deleted comments at createProjects.js

--- src/createProjects.js
+++ src/createProjects.js
@@ -10,18 +10,7 @@ function createProjects() {
 	// let dataProject = document.querySelector('[data-project]');
 	// let addProjectButton = document.getElementById('addProjectButton');
 	
-
-	// let projects = [{
-	// 	id: 1,
-	// 	names: 'Project 1'
-	// }, {
-	// 	id: 2,
-	// 	names: 'Project 2'
-	// }];
-
 	newProjectForm.addEventListener('submit', addProject);
-
-
 
 	//create add projects fxn
 
@@ -29,17 +18,14 @@ function createProjects() {
 		e.preventDefault();
 		const projectName = newProjectInput.value;
 		if (projectName === null || projectName === '') return;
-		const project = createProject(projectName);
+		createProject(projectName);
 		newProjectInput.value = null;
-		// projects.push(project);
-
 	}
 
 	function createProject(names) {
 		let project = document.createElement('li');
 		project.classList.add('list-name');
 		project.setAttribute('data-project', names);
-		
 		
 		let projectImg = document.createElement('img');
 		projectImg.classList.add('list-img');
@@ -54,9 +40,6 @@ function createProjects() {
 
 		projectsContainer.append(project);
 		addMarginToTasks();
-		
-		console.log("works", project);	
-
 	}
 
 	//add margin to tasks when a new project is added
@@ -65,10 +48,7 @@ function createProjects() {
 		let newHeight = parseInt(getHeight) + 60;
 		let heightInStr = newHeight.toString();
 		taskList.style.marginTop = heightInStr + 'px';
-
-		console.log(getHeight, newHeight, heightInStr);
 	}
-
 }
 
 export default createProjects;
