488f1f566842d20e28638228d2b754d7cd9e145e
Harmeet Matharoo <hmjatt@gmail.com>
2022-05-10 23:36:38 -0600

    removed removeMarginFromTasks()
    removed removeMarginFromTasks() at deleteProjects.js

--- src/deleteProjects.js
+++ src/deleteProjects.js
@@ -12,17 +12,8 @@ function deleteProject() {
 	document.body.addEventListener( 'click', function ( event ) {
 		if( event.target.className == 'deleteProjectBtn' ) {
 			event.target.parentElement.remove();
-			// removeMarginFromTasks();
 		};
 	});
-
-	// //remove margin from tasks when a project is deleted
-	// function removeMarginFromTasks() {
-	// 	let getHeight = getComputedStyle(taskList).marginTop;
-	// 	let newHeight = parseInt(getHeight) - 60;
-	// 	let heightInStr = newHeight.toString();
-	// 	taskList.style.marginTop = heightInStr + 'px';
-	// }
 	
 }
 
