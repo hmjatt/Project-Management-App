b626027d1605604c68c99ef54e9b78bec214cafd
Harmeet Matharoo <hmjatt@gmail.com>
2022-05-13 00:11:36 -0600

    projects are not duplicated(Bug fixed)

--- src/createProjects.js
+++ src/createProjects.js
@@ -1,60 +1,92 @@
-// import DOMInterface from './DOMInterface.js';
-// import deleteProjects from './deleteProjects.js';
+import DOMInterface from './DOMInterface.js';
+import deleteProjects from './deleteProjects.js';
 
-// document.addEventListener('DOMContentLoaded', DOMInterface);
-// document.addEventListener('DOMContentLoaded', deleteProjects);
-
-
+document.addEventListener('DOMContentLoaded', DOMInterface);
+document.addEventListener('DOMContentLoaded', deleteProjects);
 
 
-// function createProjects() {
 
-// 	newProjectForm.addEventListener('submit', addProject);
-// 	// newProjectForm.addEventListener('submit', saveProjects);
-// 	// newProjectForm.addEventListener('submit', myfunction);
 
-// 	//create add projects fxn
+function createProjects() {
 
-// 	function addProject(e) {
-// 		e.preventDefault();
-// 		const projectName = projectInput.value;
-// 		if (projectName === null || projectName === '') return;
-// 		createProject(projectName);
-// 		projectInput.value = null;
-// 	}
 
-// 	function createProject(names) {
-// 		let project = document.createElement('li');
-// 		project.classList.add('list-name');
-// 		project.setAttribute('data-project', names);
+	newProjectForm.addEventListener('submit', addProject);
+
+
+	// add new project to local storage
+	function addProject(event) {
+		event.preventDefault();
+		let newProject = projectInput.value;
+		let newProjectObj = {
+			projectName: newProject
+		};
+		let newProjectString = JSON.stringify(newProjectObj);
+		localStorage.setItem(newProject, newProjectString);
+		console.log(localStorage);
+		projectInput.value = '';
+
 		
-// 		let projectImg = document.createElement('img');
-// 		projectImg.classList.add('list-img');
-// 		projectImg.src = '../src/images/svg/list.svg';
-// 		projectImg.alt = 'list';
 
-// 		let deleteProjectButton = document.createElement('button');
-// 		deleteProjectButton.classList.add('deleteProjectBtn');
-// 		deleteProjectButton.innerText = 'X';
-
-// 		project.append(projectImg, names, deleteProjectButton);
-
-// 		projectsContainer.append(project);
-
-// 		saveProjects();
-
-// 	}
-
-// 	//add new projects to local storage
-// 	function saveProjects() {
-// 		// let newProjects = [];
-
-// 		localStorage.setItem(projectInput.value, projectInput.value);
-// 		console.log(localStorage);
-// 	}
+		loadProjects();
+		
+	}
 
 
+	
+	//load projects when new project is added
+	function loadProjects() {
+		let projects = Object.keys(localStorage);
+		projects.forEach(function(project) {
+			//if project already exists, don't add it again
+			if (document.getElementById(project)) {
+				return;
+			} else {
+				createProject(project);
+			}
+		});
+		
 
-// }
+		// for (let project of projects) {
+		// 	//if project already exists, don't add it again
+			
+		// 	createProject(project);
+		// }
+	}
 
-// export default createProjects;
+	function createProject(projectName) {
+		// if project already exists, don't add it again
+		
+
+
+
+		let project = document.createElement('li');
+		project.classList.add('list-name');
+		project.setAttribute('data-project', projectName);
+		project.id = projectName;
+		
+		let projectImg = document.createElement('img');
+		projectImg.classList.add('list-img');
+		projectImg.src = '../src/images/svg/list.svg';
+		projectImg.alt = 'list';
+
+		let deleteProjectButton = document.createElement('button');
+		deleteProjectButton.classList.add('deleteProjectBtn');
+		deleteProjectButton.innerText = 'X';
+
+		project.append(projectImg, projectName, deleteProjectButton);
+
+		projectsContainer.append(project);
+
+		
+
+
+	}
+
+	// load projects from localStorage
+	loadProjects();
+
+
+
+}
+
+export default createProjects;
