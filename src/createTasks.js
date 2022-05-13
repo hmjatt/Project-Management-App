fe6996c0a7266276b77856f669498bd8ddff969d
Harmeet Matharoo <hmjatt@gmail.com>
2022-05-13 00:30:00 -0600

    deleted unnecessary comments in html

---
+++ src/createTasks.js
@@ -0,0 +1,18 @@
+import DOMInterface from './DOMInterface.js';
+// import createProjects from './createProjects.js';
+// import deleteProjects from './deleteProjects.js';
+
+document.addEventListener('DOMContentLoaded', DOMInterface);
+// document.addEventListener('DOMContentLoaded', createProjects);
+// document.addEventListener('DOMContentLoaded', deleteProjects);
+
+
+function createTasks {
+
+	//create new task
+	
+
+
+}
+
+export default createTasks;
