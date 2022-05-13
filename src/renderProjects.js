1445ad2c14072ed4ef446e22f3fabdda9941d73c
Harmeet Matharoo <hmjatt@gmail.com>
2022-05-12 19:48:30 -0600

    created default projects dynamically

--- src/renderProjects.js
+++ src/renderProjects.js
@@ -32,81 +32,157 @@ function renderProjects() {
 
 
 	// create default tasks
-	let youtubeProjectTasks = `
-						<div class="task">
-							<input type="checkbox" id="task-1" name="task-1" value="task"/>
-							<label for="task-1" data-content="Record todo list video that is long">
-								Record todo list video that is long
-							</label>
-						</div>
 
-						<div class="task">
-							<input type="checkbox" id="task-2" name="task-2" value="task"/>
-							<label for="task-2" data-content="Another Task">
-								Another Task
-							</label>
-						</div>
+	// Youtube
+	let youtubeProjectTask1 = document.createElement('div');
+	youtubeProjectTask1.classList.add('task');
+	let youtubeProjectTask1Input = document.createElement('input');
+	youtubeProjectTask1Input.id = 'task-1';
+	youtubeProjectTask1Input.setAttribute('type', 'checkbox');
+	youtubeProjectTask1Input.setAttribute('name', 'task-1');
+	youtubeProjectTask1Input.setAttribute('value', 'task');
 
-						<div class="task">
-							<input type="checkbox" id="task-3" name="task-3" value="task"/>
-							<label for="task-3" data-content="Third Task">
-								Third Task
-							</label>
-						</div>
-					`;
+	let youtubeProjectTask1Label = document.createElement('label');
+	youtubeProjectTask1Label.setAttribute('for', 'task-1');
+	youtubeProjectTask1Label.setAttribute('data-content', 'Record todo list video that is long');
+	youtubeProjectTask1Label.innerText = 'Record todo list video that is long';
 
-	let workProjectTasks = `
-						<div class="task">
-							<input type="checkbox" id="task-4" name="task-4" value="task"/>
-							<label for="task-4" data-content="Code a new project">
-								Code a new project
-							</label>
-						</div>
-
-						<div class="task">
-							<input type="checkbox" id="task-5" name="task-5" value="task"/>
-							<label for="task-5" data-content="Check E-mail">
-								Check E-mail
-							</label>
-						</div>
-
-						<div class="task">
-							<input type="checkbox" id="task-6" name="task-6" value="task"/>
-							<label for="task-6" data-content="Call John Doe">
-								Call John Doe
-							</label>
-						</div>
-					`;
-
-	let groceryProjectTasks = `
-	
-						<div class="task">
-							<input type="checkbox" id="task-7" name="task-7" value="task"/>
-							<label for="task-7" data-content="Buy milk">
-								Buy milk
-							</label>
-						</div>
-
-						<div class="task">
-							<input type="checkbox" id="task-8" name="task-8" value="task"/>
-							<label for="task-8" data-content="Buy eggs">
-								Buy eggs
-							</label>
-						</div>
-
-						<div class="task">
-							<input type="checkbox" id="task-9" name="task-9" value="task"/>
-							<label for="task-9" data-content="Buy bread">
-								Buy bread
-							</label>
-						</div>
-					`;
-
-							
+	youtubeProjectTask1.append(youtubeProjectTask1Input, youtubeProjectTask1Label);
 
 
-	tasksContainer.innerHTML = `${youtubeProjectTasks}`;
-	projectTitle.innerText = "Youtube";
+	let youtubeProjectTask2 = document.createElement('div');
+	youtubeProjectTask2.classList.add('task');
+	let youtubeProjectTask2Input = document.createElement('input');
+	youtubeProjectTask2Input.id = 'task-2';
+	youtubeProjectTask2Input.setAttribute('type', 'checkbox');
+	youtubeProjectTask2Input.setAttribute('name', 'task-2');
+	youtubeProjectTask2Input.setAttribute('value', 'task');
+
+	let youtubeProjectTask2Label = document.createElement('label');
+	youtubeProjectTask2Label.setAttribute('for', 'task-2');
+	youtubeProjectTask2Label.setAttribute('data-content', 'Subscribe to youtube channel');
+	youtubeProjectTask2Label.innerText = 'Subscribe to youtube channel';
+
+	youtubeProjectTask2.append(youtubeProjectTask2Input, youtubeProjectTask2Label);
+
+
+	let youtubeProjectTask3 = document.createElement('div');
+	youtubeProjectTask3.classList.add('task');
+	let youtubeProjectTask3Input = document.createElement('input');
+	youtubeProjectTask3Input.id = 'task-3';
+	youtubeProjectTask3Input.setAttribute('type', 'checkbox');
+	youtubeProjectTask3Input.setAttribute('name', 'task-3');
+	youtubeProjectTask3Input.setAttribute('value', 'task');
+
+	let youtubeProjectTask3Label = document.createElement('label');
+	youtubeProjectTask3Label.setAttribute('for', 'task-3');
+	youtubeProjectTask3Label.setAttribute('data-content', 'Like Video');
+	youtubeProjectTask3Label.innerText = 'Like Video';
+
+	youtubeProjectTask3.append(youtubeProjectTask3Input, youtubeProjectTask3Label);
+
+
+
+	// Work
+	let workProjectTask1 = document.createElement('div');
+	workProjectTask1.classList.add('task');
+	let workProjectTask1Input = document.createElement('input');
+	workProjectTask1Input.id = 'task-4';
+	workProjectTask1Input.setAttribute('type', 'checkbox');
+	workProjectTask1Input.setAttribute('name', 'task-4');
+	workProjectTask1Input.setAttribute('value', 'task');
+
+	let workProjectTask1Label = document.createElement('label');
+	workProjectTask1Label.setAttribute('for', 'task-4');
+	workProjectTask1Label.setAttribute('data-content', 'Create a new project');
+	workProjectTask1Label.innerText = 'Create a new project';
+
+	workProjectTask1.append(workProjectTask1Input, workProjectTask1Label);
+
+
+	let workProjectTask2 = document.createElement('div');
+	workProjectTask2.classList.add('task');
+	let workProjectTask2Input = document.createElement('input');
+	workProjectTask2Input.id = 'task-5';
+	workProjectTask2Input.setAttribute('type', 'checkbox');
+	workProjectTask2Input.setAttribute('name', 'task-5');
+	workProjectTask2Input.setAttribute('value', 'task');
+
+	let workProjectTask2Label = document.createElement('label');
+	workProjectTask2Label.setAttribute('for', 'task-5');
+	workProjectTask2Label.setAttribute('data-content', 'Check Email');
+	workProjectTask2Label.innerText = 'Check Email';
+
+	workProjectTask2.append(workProjectTask2Input, workProjectTask2Label);
+
+
+	let workProjectTask3 = document.createElement('div');
+	workProjectTask3.classList.add('task');
+	let workProjectTask3Input = document.createElement('input');
+	workProjectTask3Input.id = 'task-6';
+	workProjectTask3Input.setAttribute('type', 'checkbox');
+	workProjectTask3Input.setAttribute('name', 'task-6');
+	workProjectTask3Input.setAttribute('value', 'task');
+
+	let workProjectTask3Label = document.createElement('label');
+	workProjectTask3Label.setAttribute('for', 'task-6');
+	workProjectTask3Label.setAttribute('data-content', 'Call John Doe');
+	workProjectTask3Label.innerText = 'Call John Doe';
+
+	workProjectTask3.append(workProjectTask3Input, workProjectTask3Label);
+
+
+	// Grocery
+	let groceryProjectTask1 = document.createElement('div');
+	groceryProjectTask1.classList.add('task');
+	let groceryProjectTask1Input = document.createElement('input');
+	groceryProjectTask1Input.id = 'task-7';
+	groceryProjectTask1Input.setAttribute('type', 'checkbox');
+	groceryProjectTask1Input.setAttribute('name', 'task-7');
+	groceryProjectTask1Input.setAttribute('value', 'task');
+
+	let groceryProjectTask1Label = document.createElement('label');
+	groceryProjectTask1Label.setAttribute('for', 'task-7');
+	groceryProjectTask1Label.setAttribute('data-content', 'Buy milk');
+	groceryProjectTask1Label.innerText = 'Buy milk';
+
+	groceryProjectTask1.append(groceryProjectTask1Input, groceryProjectTask1Label);
+
+
+	let groceryProjectTask2 = document.createElement('div');
+	groceryProjectTask2.classList.add('task');
+	let groceryProjectTask2Input = document.createElement('input');
+	groceryProjectTask2Input.id = 'task-8';
+	groceryProjectTask2Input.setAttribute('type', 'checkbox');
+	groceryProjectTask2Input.setAttribute('name', 'task-8');
+	groceryProjectTask2Input.setAttribute('value', 'task');
+
+	let groceryProjectTask2Label = document.createElement('label');
+	groceryProjectTask2Label.setAttribute('for', 'task-8');
+	groceryProjectTask2Label.setAttribute('data-content', 'Buy bread');
+	groceryProjectTask2Label.innerText = 'Buy bread';
+
+	groceryProjectTask2.append(groceryProjectTask2Input, groceryProjectTask2Label);
+
+
+	let groceryProjectTask3 = document.createElement('div');
+	groceryProjectTask3.classList.add('task');
+	let groceryProjectTask3Input = document.createElement('input');
+	groceryProjectTask3Input.id = 'task-9';
+	groceryProjectTask3Input.setAttribute('type', 'checkbox');
+	groceryProjectTask3Input.setAttribute('name', 'task-9');
+	groceryProjectTask3Input.setAttribute('value', 'task');
+
+	let groceryProjectTask3Label = document.createElement('label');
+	groceryProjectTask3Label.setAttribute('for', 'task-9');
+	groceryProjectTask3Label.setAttribute('data-content', 'Buy eggs');
+	groceryProjectTask3Label.innerText = 'Buy eggs';
+
+	groceryProjectTask3.append(groceryProjectTask3Input, groceryProjectTask3Label);
+
+
+	tasksContainer.append(youtubeProjectTask1, youtubeProjectTask2, youtubeProjectTask3);
+	projectTitle.append(youtubeProject.getAttribute('data-project'));
 
 
 	//select project and render its tasks
@@ -121,18 +197,24 @@ function renderProjects() {
 			console.log(event.target.getAttribute('data-project'));
 			// let projectTasks = document.querySelector(`[data-project="${projectName}"]`);
 
-			// tasksContainer.innerHTML = `${youtubeProjectTasks}`;
+			// tasksContainer.innerHTML = `${youtubeProjectTask1}`;
 			
 
 			if(projectName == 'Youtube') {
-				tasksContainer.innerHTML = `${youtubeProjectTasks}`;
-				projectTitle.innerHTML = `${projectName}`;
+				tasksContainer.innerHTML = '';
+				projectTitle.innerHTML = '';
+				tasksContainer.append(youtubeProjectTask1, youtubeProjectTask2, youtubeProjectTask3);
+				projectTitle.append(youtubeProject.getAttribute('data-project'));
 			} else if(projectName == 'Work') {
-				tasksContainer.innerHTML = `${workProjectTasks}`;
-				projectTitle.innerHTML = `${projectName}`;
+				tasksContainer.innerHTML = '';
+				projectTitle.innerHTML = '';
+				tasksContainer.append(workProjectTask1, workProjectTask2, workProjectTask3);
+				projectTitle.append(workProject.getAttribute('data-project'));
 			} else if(projectName == 'Grocery') {
-				tasksContainer.innerHTML = `${groceryProjectTasks}`;
-				projectTitle.innerHTML = `${projectName}`;
+				tasksContainer.innerHTML = '';
+				projectTitle.innerHTML = '';
+				tasksContainer.append(groceryProjectTask1, groceryProjectTask2, groceryProjectTask3);
+				projectTitle.append(groceryProject.getAttribute('data-project'));
 			} else {
 				projectTitle.innerHTML = `${projectName}`;
 				tasksContainer.innerHTML = `<div id="noTasks" class="task">` + `No tasks for this project` + `</div>`;
