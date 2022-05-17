0c6cef9b9b5b40c92f95d49414187943337662ac
Harmeet Matharoo <hmjatt@gmail.com>
2022-05-17 01:03:13 -0600

    tasks localstorage wip

--- src/createTasks.js
+++ src/createTasks.js
@@ -11,6 +11,118 @@ document.addEventListener('DOMContentLoaded', renderTasks);
 
 function createTasks() {
 
+	//create new task
+	
+	// let projectName = event.target.getAttribute('data-project');
+
+	// //grab project name from data-project attribute
+	// document.body.addEventListener( 'click', function ( event ) {
+	// 	if( event.target.className == 'list-name active-list' ) {
+	// 		let projectName = event.target.getAttribute('data-project');
+	// 		// console.log(projectName, localStorage);
+
+	// 		newTaskForm.addEventListener('submit', addTask);
+
+	// 		// when i hit submit on new task form, add task to local storage with project name as key
+
+	// 		function addTask(event) {
+	// 			event.preventDefault();
+	// 			let newTask = taskInput.value;
+	// 			let newTaskObj = {
+	// 				newTask: newTask
+	// 			};
+	// 			let newTaskString = JSON.stringify(newTaskObj);
+	// 			localStorage.setItem(projectName, newTaskString);
+	// 			taskInput.value = '';
+
+	// 			loadTasks();
+	// 		}
+
+	// 		//load tasks when new task is added
+	// 		function loadTasks() {
+	// 			let tasks = localStorage.getItem(projectName);
+	// 			console.log(typeof(tasks));
+	// 			// tasks.forEach(function (task) {
+	// 			// 	//if task already exists, don't add it again
+	// 			// 	// if (document.getElementById(task)) {
+	// 			// 	// 	return;
+	// 			// 	// } else {
+	// 			// 	// 	createTask(task);
+	// 			// 	// }
+
+					
+	// 			// });
+	// 			createTask(tasks);
+				
+	// 		}
+
+	// 		function createTask(taskName) {
+				
+	// 			// let task = document.createElement('li');
+	// 			// task.classList.add('list-name');
+	// 			// task.setAttribute('data-task', taskName);
+	// 			// task.id = taskName;
+
+	// 			// let taskImg = document.createElement('img');
+	// 			// taskImg.classList.add('list-img');
+	// 			// taskImg.src = '../src/images/svg/list.svg';
+	// 			// taskImg.alt = 'list';
+
+	// 			// let deleteTaskButton = document.createElement('button');
+	// 			// deleteTaskButton.classList.add('deleteTaskBtn');
+
+	// 			// task.appendChild(taskImg);
+	// 			// task.appendChild(deleteTaskButton);
+	// 			// taskList.appendChild(task);
+	// 			let task = document.createElement('div');
+	// 			task.classList.add('task');
+	// 			let taskInput = document.createElement('input');
+	// 			taskInput.id = taskName;
+	// 			taskInput.type = 'checkbox';
+	// 			taskInput.name = taskName;
+	// 			taskInput.value = taskName;
+	// 			taskInput.classList.add('task-input');
+				
+	// 			let taskLabel = document.createElement('label');
+	// 			taskLabel.classList.add('task-label');
+	// 			taskLabel.setAttribute('for', taskName);
+	// 			taskLabel.innerText = taskName;
+	// 			taskLabel.setAttribute('data-task', taskName);
+
+	// 			task.append(taskInput, taskLabel);
+
+	// 			//add each task to array
+	// 			let taskArray = [];
+	// 			taskArray.push(task);
+
+
+
+	// 		}
+
+
+	// 	}
+		
+	// });
+
+
+
+	//add tasks to default project
+
+	
+
+	// //check if task is checked change it in local storage
+	// let checkboxes = document.querySelectorAll('input[type="checkbox"]');
+	// checkboxes.forEach(checkbox => {
+	// 	checkbox.addEventListener('click', () => {
+	// 		let project = checkbox.parentElement.parentElement.parentElement.parentElement.getAttribute('data-project');
+	// 		let task = checkbox.parentElement.parentElement.getAttribute('data-task');
+	// 		let checked = checkbox.checked;
+	// 		let projects = JSON.parse(localStorage.getItem('defaultProjects'));
+	// 		projects[project][task]['checked'] = checked;
+	// 		localStorage.setItem('defaultProjects', JSON.stringify(projects));
+	// 		console.log(JSON.parse(localStorage.getItem('defaultProjects')));
+	// 	});
+	// }
 
 
 
@@ -55,28 +167,21 @@ function createTasks() {
 			let project = document.querySelector('.active-list').getAttribute('data-project');
 			let task = taskInput.value;
 
-			
-
-			
+			let taskObj = {
+				[project] : task,
+			}
 
 			//add task to selected project
-
-
-
-			
-
 			if(project) {
-				
-				taskArray.push(task);
+				taskArray.push(taskObj);
 			} else {
 				let taskArray = [];
-				taskArray.push(task);
+				taskArray.push(taskObj);
 			}
 			
 			
 			
-			localStorage.setItem(project, JSON.stringify(taskArray));
-			// console.log(localStorage);
+			localStorage.setItem(project, taskArray);
 			taskInput.value = '';
 			loadTasks();
 			
@@ -87,39 +192,32 @@ function createTasks() {
 
 		//load tasks from local storage
 		const loadTasks = () => {
-			console.log(localStorage);
+			console.log(taskArray, localStorage);
 
 
+			let allTasks = taskArray.values();
 
-
-
-
-
-
-
-			// let allTasks = taskArray.values();
-
-			// for(let task of allTasks) {
+			for(let task of allTasks) {
 				
 
-			// 	let projectNames = Object.keys(task);
-			// 	let taskNames = Object.values(task);
+				let projectNames = Object.keys(task);
+				let taskNames = Object.values(task);
 
 				
 
 
-			// 	return projectNames, taskNames;
+				return projectNames, taskNames;
 
-			// }
+			}
 
-			// if(taskArray.length > 0) {
-			// 	console.log(taskArray);
+			if(taskArray.length > 0) {
+				console.log(taskArray);
 
-			// 	createNewTask(projectNames, taskNames);
-			// } else {
-			// 	console.log('no tasks');
-			// 	return;
-			// }
+				createNewTask(projectNames, taskNames);
+			} else {
+				console.log('no tasks');
+				return;
+			}
 
 			
 
